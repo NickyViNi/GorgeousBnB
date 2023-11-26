08f3f21155865f1fbdac911b4b0de26780f9c183
NickyViNi <happylemonlcm@gmail.com>
2023-11-26 14:12:45 -0800

    add User login backend endpoint

---
+++ backend/routes/api/users.js
@@ -0,0 +1,6 @@
+//This file will hold the resources for the route paths beginning with /api/users
+// backend/routes/api/users.js
+const express = require('express')
+const router = express.Router();
+
+module.exports = router;
