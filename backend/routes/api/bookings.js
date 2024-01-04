599af82972ab26af81afc1a3ba8ffae8b1ae43ba
NickyViNi <happylemonlcm@gmail.com>
2024-01-04 00:36:13 -0800

    Get all of the Current User's Bookings

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -7,12 +7,44 @@ const { User, Spot, SpotImage, Booking, Review, ReviewImage} = require('../../db
 // const { check } = require('express-validator');
 const { validateBookingDate, endDateNotBeforeStartdate, bookingDateConflict } = require('../../utils/validation');
 
-// const { Sequelize, Op } = require("sequelize");
+const { Op } = require("sequelize");
 
 const router = express.Router();
 
 //(1) GET:Get all of the Current User's Bookings, URL: /api/bookings/current
 router.get("/current", requireAuth, async (req, res) => {
+    const bookings = await Booking.unscoped().findAll({
+        where: { userId: req.user.id },
+        include: {
+            model: Spot,
+            attributes: {
+                exclude: ["description", "createdAt", "updatedAt"]
+            }
+        }
+    });
+
+    const result = [];
+    for (let booking of bookings) {
+        booking = booking.toJSON();
+        booking.Spot.previewImage = "";
+
+        const spotImage = await SpotImage.findOne({
+            where: {
+                [Op.and]: [
+                    { spotId: booking.Spot.id },
+                    { preview: true}
+                ]
+            }
+        })
+
+        if (spotImage) {
+            booking.Spot.previewImage = spotImage.url;
+        }
+
+        result.push(booking);
+    };
+
+    res.json({ Bookings: result });
 
 })
 
