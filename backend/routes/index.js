0675ca8c4d7f9f14dc7509d1369dcb8add108562
NickyViNi <happylemonlcm@gmail.com>
2024-01-19 16:18:18 -0800

    deploy set up

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,21 +7,57 @@ const apiRouter = require('./api');
 //All the URLs of the routes in the api router will be prefixed with /api
 router.use('/api', apiRouter);
 
-// Add a XSRF-TOKEN cookie
-//to allow any developer to re-set the CSRF token cookie XSRF-TOKEN
-router.get("/api/csrf/restore", (req, res) => {
+// Static routes
+// Serve React build files in production
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  // Serve the frontend's index.html file at the root route
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
+    );
+  });
+
+  // Serve the static assets in the frontend's build folder
+  router.use(express.static(path.resolve("../frontend/dist")));
+
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
+    );
+  });
+}
+
+// Add a XSRF-TOKEN cookie in development
+if (process.env.NODE_ENV !== 'production') {
+  router.get("/api/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
     res.cookie("XSRF-TOKEN", csrfToken);
     res.status(200).json({
       'XSRF-Token': csrfToken
     });
   });
+}
+
+
+// Add a XSRF-TOKEN cookie
+//to allow any developer to re-set the CSRF token cookie XSRF-TOKEN
+// router.get("/api/csrf/restore", (req, res) => {
+//     const csrfToken = req.csrfToken();
+//     res.cookie("XSRF-TOKEN", csrfToken);
+//     res.status(200).json({
+//       'XSRF-Token': csrfToken
+//     });
+//   });
 
 //test the reset XSRF_Token: http://localhost:8000/hello/world
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
+// router.get('/hello/world', function(req, res) {
+//   res.cookie('XSRF-TOKEN', req.csrfToken());
+//   res.send('Hello World!');
+// });
 
 
 
