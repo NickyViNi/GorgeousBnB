ec1677d7cb17a3957d4816c7830fdc3b3eb08d34
NickyViNi <happylemonlcm@gmail.com>
2024-01-22 01:03:36 -0800

    updated users seeder

--- backend/db/seeders/20231126090432-demo-user.js
+++ backend/db/seeders/20231126090432-demo-user.js
@@ -10,6 +10,70 @@ if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;
 }
 
+const users = [
+  {
+    email: 'davecode@user.io',
+    username: 'daveCode',
+    hashedPassword: bcrypt.hashSync('password1'),
+    firstName: 'Dave',
+    lastName: 'Code'
+  },
+  {
+    email: 'mattthooy@user.io',
+    username: 'mattThooy',
+    hashedPassword: bcrypt.hashSync('password2'),
+    firstName: 'Matt',
+    lastName: 'Thooy'
+  },
+  {
+    email: 'dennihook@user.io',
+    username: 'denniHook',
+    hashedPassword: bcrypt.hashSync('password3'),
+    firstName: 'Denni',
+    lastName: 'Hook'
+  },
+
+  {
+    email: "vince@gmail.com",
+    username: "vinceStar",
+    hashedPassword: bcrypt.hashSync('password520'),
+    firstName: "Vince",
+    lastName: "Star"
+ },
+
+ {
+  email: "sillvina@gmail.com",
+  username: "sillvinaKing",
+  hashedPassword: bcrypt.hashSync('password521'),
+    firstName: "Sillvina",
+    lastName: "King"
+  },
+
+  {
+    email: "iloveyou@gmail.com",
+    username: "cocoFresh",
+    hashedPassword: bcrypt.hashSync('password522'),
+    firstName: "Coco",
+    lastName: "Fresh"
+  },
+
+  {
+    email: "brianballard@gmail.com",
+    username: "brianBallard",
+    hashedPassword: bcrypt.hashSync('password523'),
+    firstName: "Brian",
+    lastName: "Ballard"
+  },
+
+  {
+    email: "lancemoreno@gmail.com",
+    username: "lanceMoreno",
+    hashedPassword: bcrypt.hashSync('password524'),
+    firstName: "Lance",
+    lastName: "Moreno"
+  },
+]
+
 module.exports = {
   async up (queryInterface, Sequelize) {
     /**
@@ -21,47 +85,7 @@ module.exports = {
      *   isBetaMember: false
      * }], {});
     */
-    await User.bulkCreate([
-      {
-        email: 'davecode@user.io',
-        username: 'daveCode',
-        hashedPassword: bcrypt.hashSync('password1'),
-        firstName: 'Dave',
-        lastName: 'Code'
-      },
-      {
-        email: 'mattthooy@user.io',
-        username: 'mattThooy',
-        hashedPassword: bcrypt.hashSync('password2'),
-        firstName: 'Matt',
-        lastName: 'Thooy'
-      },
-      {
-        email: 'dennihook@user.io',
-        username: 'denniHook',
-        hashedPassword: bcrypt.hashSync('password3'),
-        firstName: 'Denni',
-        lastName: 'Hook'
-      },
-
-      {
-        email: "vince@gmail.com",
-        username: "vinceStar",
-        hashedPassword: bcrypt.hashSync('password520'),
-        firstName: "Vince",
-        lastName: "Star"
-     },
-
-     {
-      email: "sillvina@gmail.com",
-      username: "sillvinaKing",
-      hashedPassword: bcrypt.hashSync('password521'),
-      firstName: "Sillvina",
-      lastName: "King"
-   },
-
-
-    ], { validate: true });
+    await User.bulkCreate(users, { validate: true });
   },
 
   async down (queryInterface, Sequelize) {
@@ -74,7 +98,7 @@ module.exports = {
     options.tableName = 'Users';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      username: { [Op.in]: ['daveCode', 'mattThooy', 'denniHook', "vinceStar", "sillvinaKing"] }
+      username: { [Op.in]: users.map((user) => user.username) }
     }, {});
   }
 };
--- backend/db/seeders/20231128103840-spot-seeder.js
+++ backend/db/seeders/20231128103840-spot-seeder.js
@@ -10,82 +10,70 @@ if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;
 }
 
+const spotsSeeder = [
+  {
+    ownerId: 1,
+    address: '16384 CT NE Seattle 98101',
+    city: 'Seattle',
+    state: 'Washington',
+    country: 'United States of America',
+    lat: 23.6061,
+    lng: 99.3328,
+    name: "Happy Home",
+    description: "Welcome to Happy Home, enjoy your time!",
+    price: 325
+  },
+  {
+    ownerId: 2,
+    address: '1356 CT NE Bellevue 98004',
+    city: 'Bellevue',
+    state: 'Washington',
+    country: 'United States of America',
+    lat: 78.6101,
+    lng: 150.2015,
+    name: "Golden Holiday",
+    description: "Welcome to Golden Holiday, enjoy your time!",
+    price: 199
+  },
+  {
+    ownerId: 3,
+    address: '1999 CT NE Kirkland 98034',
+    city: 'Kirkland',
+    state: 'Washington',
+    country: 'United States of America',
+    lat: 51.6769,
+    lng: 50.2060,
+    name: "Gorgeous Hotel",
+    description: "Welcome to Gorgeous Hotel, enjoy your time!",
+    price: 188
+  },
+  {
+    ownerId: 4,
+    address: '2888 CT NE Redmond 98052',
+    city: 'Redmond',
+    state: 'Washington',
+    country: 'United States of America',
+    lat: 37.6740,
+    lng: 39.1215,
+    name: "Fantastic Castle",
+    description: "Welcome to Fantastic Castle, enjoy your time!",
+    price: 598
+  }
+]
+
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-    await Spot.bulkCreate([
-      {
-        ownerId: 1,
-        address: '16384 CT NE Seattle 98101',
-        city: 'Seattle',
-        state: 'Washington',
-        country: 'United States of America',
-        lat: 23.6061,
-        lng: 99.3328,
-        name: "Happy Home",
-        description: "Welcome to Happy Home, enjoy your time!",
-        price: 325
-      },
-      {
-        ownerId: 2,
-        address: '1356 CT NE Bellevue 98004',
-        city: 'Bellevue',
-        state: 'Washington',
-        country: 'United States of America',
-        lat: 78.6101,
-        lng: 150.2015,
-        name: "Golden Holiday",
-        description: "Welcome to Golden Holiday, enjoy your time!",
-        price: 199
-      },
-      {
-        ownerId: 3,
-        address: '1999 CT NE Kirkland 98034',
-        city: 'Kirkland',
-        state: 'Washington',
-        country: 'United States of America',
-        lat: 51.6769,
-        lng: 50.2060,
-        name: "Gorgeous Hotel",
-        description: "Welcome to Gorgeous Hotel, enjoy your time!",
-        price: 188
-      },
-      {
-        ownerId: 4,
-        address: '2888 CT NE Redmond 98052',
-        city: 'Redmond',
-        state: 'Washington',
-        country: 'United States of America',
-        lat: 37.6740,
-        lng: 39.1215,
-        name: "Fantastic Castle",
-        description: "Welcome to Fantastic Castle, enjoy your time!",
-        price: 598
-      }
 
-    ], { validate: true });
+    await Spot.bulkCreate( spotsSeeder, { validate: true });
 
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+
     options.tableName = 'Spots';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      name: { [Op.in]: ['Happy Home', 'Golden Holiday', 'Gorgeous Hotel', "Fantastic Castle"] }
+      name: { [Op.in]: spotsSeeder.map(spot => spot.name) }
     }, {});
   }
 };
