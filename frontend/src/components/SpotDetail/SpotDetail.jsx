1e619e4086a18894e26d958af740e0b84ef2be99
NickyViNi <happylemonlcm@gmail.com>
2024-01-23 10:58:36 -0800

    add favicon

--- frontend/src/components/SpotDetail/SpotDetail.jsx
+++ frontend/src/components/SpotDetail/SpotDetail.jsx
@@ -1,5 +1,12 @@
+// import { useParams, useNavigate } from 'react-router-dom';
+// import { useState } from 'react';
 
 function SpotDetail () {
+
+    // const { spotId } = useParams();
+    // const navigate = useNavigate();
+
+
     return (
         <h1>Hiii</h1>
     )
