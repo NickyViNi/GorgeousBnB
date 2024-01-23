0b15167e8f26be82ec12341ed28646716759d3f1
NickyViNi <happylemonlcm@gmail.com>
2024-01-23 14:41:58 -0800

    add review thunk

--- frontend/src/store/review.js
+++ frontend/src/store/review.js
@@ -4,14 +4,35 @@ import { csrfFetch } from "./csrf";
 const GET_REVIEWS_BY_SPOTID = 'reviews/getReviewsBySpotId';
 
 //action creator:
-export const getReviewsBySpotIdAction = (spot) => {
+export const getReviewsBySpotIdAction = (reviews) => {
     return {
         type: GET_REVIEWS_BY_SPOTID,
-        spot
+        reviews
     }
 }
 
 // thunk
-export const getReviewsBySpotIdThunk = () => async (dispatch) => {
-    const res = await csrfFetch()
+export const getReviewsBySpotIdThunk = (spotId) => async (dispatch) => {
+    const res = await csrfFetch(`/api/spots/${spotId}/reviews`);
+    const data = res.json();
+    if(res.ok) {
+        const reviews = data.Reviews;
+        dispatch(getReviewsBySpotIdAction(reviews))
+    }
+    return data;
+}
+
+//reducer:
+const initialState = { spotReviews: {}, userReviews: {} }
+export default function reviewsReducer (state = initialState, action)  {
+    switch (action.type) {
+        case GET_REVIEWS_BY_SPOTID: {
+            const newSpotReviews = {};
+            action.reviews.forEach(rev => newSpotReviews[rev.id] = rev);
+            const newState = {...state, spotReviews: newSpotReviews}
+            return newState;
+        }
+        default:
+            return state;
+    }
 }
