468f0a84b309cf0b307c8473657eb42ca9e41c4f
NickyViNi <happylemonlcm@gmail.com>
2024-01-24 11:13:52 -0800

    add stars to every review

--- frontend/src/components/ReviewList/ReviewTile.jsx
+++ frontend/src/components/ReviewList/ReviewTile.jsx
@@ -5,18 +5,19 @@ export default function ReviewTile({review}) {
     // const reviewYear = new Date(review.createdAt).getFullYear();
     const solidStar = <i className="fas fa-star solidStar"/>
     const emptyStar = <i className="far fa-star emptyStar"/>
+    const generateStars = () => {
+        const stars = new Array(review.stars).fill(solidStar);
+        for (let i = 0; i < 5 - review.stars; i++) {
+            stars.push(emptyStar);
+        }
+        return stars;
+    }
 
     return (
         <div className="review-tile">
             <h3>
                 {review.User.firstName} <span>&#183;</span>
-                {(() => {
-                    const stars = new Array(review.stars).fill(solidStar);
-                    for (let i = 0; i < 5 - review.stars; i++) {
-                        stars.push(emptyStar);
-                    }
-                    return stars;
-                })()}
+                {generateStars()}
             </h3>
             <h4>{reviewMonthYear}</h4>
             <p>{review.review}</p>
