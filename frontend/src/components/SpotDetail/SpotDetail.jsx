a05b8bff2827fab50313bc5d3f39a7ebaaa3eca9
NickyViNi <happylemonlcm@gmail.com>
2024-01-23 21:44:59 -0800

    spot details

--- frontend/src/components/SpotDetail/SpotDetail.jsx
+++ frontend/src/components/SpotDetail/SpotDetail.jsx
@@ -15,7 +15,12 @@ function SpotDetail () {
     const currSpotReviews = useSelector(state => state.reviews.spotReviews);
 
     const reviewsArr = Object.values(currSpotReviews);
-    const currSpotAvgRating = reviewsArr.reduce((accumulator, currReview) => accumulator + currReview.stars, 0) / reviewsArr.length;
+    const reviewsLength = reviewsArr.length;
+
+    let currSpotAvgRating = 'New';
+    if (reviewsLength) {
+        currSpotAvgRating =parseFloat(reviewsArr.reduce((accumulator, currReview) => accumulator + currReview.stars, 0) / reviewsLength).toFixed(1);
+    }
 
     useEffect(() => {
         dispatch(getSpotByIdThunk(parseInt(spotId)));
@@ -38,6 +43,40 @@ function SpotDetail () {
                 <h3>{currentSpot.city}, {currentSpot.state}, {currentSpot.country}</h3>
             </div>
             <SpotImagesList spotImages={currentSpot.SpotImages} />
+
+            <div id='spot-callout-container' >
+                <div id='spot-details-container' >
+                    <div id='spot-owner-info' >
+                        <h2>Hosted by {currentSpot.Owner.firstName} {currentSpot.Owner.lastName}</h2>
+                    </div>
+                    <div id='spot-description' >
+                        <p>{currentSpot.description}</p>
+                    </div>
+                </div>
+
+                <div id='spot-callout-info' >
+                    <div id='price-reviews-container'>
+                        <div id='spot-price'>
+                            <h2>${currentSpot.price}</h2>
+                            <span>night</span>
+                        </div>
+                        <div id='star-rating'>
+                            <i className='fas fa-star'></i>
+                            <span id='ave-rating'>{currSpotAvgRating}</span>
+                            <span id='center-spot'>
+                                {reviewsLength > 0 && <span>&#183;</span>}
+                            </span>
+                            {reviewsLength === 1 && <span>{reviewsLength} Review</span>}
+                            {reviewsLength > 1 && <span>{reviewsLength} Reviews</span>}
+                        </div>
+                    </div>
+                    <button onClick={reserveAlert} id='reserve-button'>
+                        Reserve
+                    </button>
+                </div>
+            </div>
+
+            <div className='separator'></div>
         </div>
     )
 }
