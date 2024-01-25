230c1e5b364b44427f29f29e0872387b9e2d081e
NickyViNi <happylemonlcm@gmail.com>
2024-01-25 02:00:23 -0800

    manage spot tile

---
+++ frontend/src/components/ManageSpots/ManageSpotTile.jsx
@@ -0,0 +1,26 @@
+import { useNavigate } from "react-router-dom";
+
+export default function ManageSpotTile ({spot}) {
+    const navigate = useNavigate();
+
+    return (
+        <div className="manage-spot-tile-container" title={spot.name} >
+            <div className="spot-image" onClick={() => navigate(`/spots/${spot.id}`)}>
+                <img className="image" src={spot.previewImage} />
+            </div>
+            <div className="spot-information" onClick={() => navigate(`/spots/${spot.id}`)} >
+                <div className="city-state-rating">
+                    <div className="city-state">
+                        {`${spot.city}, ${spot.state}`}
+                    </div>
+                    <div className="star-rating">
+                        <i className="fas fa-star"></i>
+                        {spot.avgRating}
+                    </div>
+                </div>
+                <span className="price-span">$ {spot.price}</span>
+                <span className="night-span">night</span>
+            </div>
+        </div>
+    )
+}
