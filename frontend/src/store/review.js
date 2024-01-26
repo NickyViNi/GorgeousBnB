a6b19ef8bdb1abb1521d92996aa8184d69b1c0cc
NickyViNi <happylemonlcm@gmail.com>
2024-01-25 23:52:18 -0800

    add delete review thunk

--- frontend/src/store/review.js
+++ frontend/src/store/review.js
@@ -1,13 +1,24 @@
 import { csrfFetch } from "./csrf";
+import { getSpotByIdAction, updateSpotAction } from "./spots";
 
 //action types:
 const GET_REVIEWS_BY_SPOTID = 'reviews/getReviewsBySpotId';
+const DELETE_REVIEW = 'reviews/deleteReview';
+const CREATE_REVIEW = 'reviews/createReview';
+// const GET_CURRENT_USER_REVIEWS = 'reviews/getCurrentUserReviews';
 
 //action creator:
 export const getReviewsBySpotIdAction = (reviews) => {
     return {
         type: GET_REVIEWS_BY_SPOTID,
         reviews
+    }
+}
+
+export const deleteReviewAction = (reviewId) => {
+    return {
+        type: DELETE_REVIEW,
+        reviewId
     }
 }
 
@@ -24,8 +35,28 @@ export const getReviewsBySpotIdThunk = (spotId) => async (dispatch) => {
     return data;
 }
 
+export const deleteReviewThunk = (reviewId, spotId) => async (dispatch) => {
+    //delete review
+    const res = await csrfFetch(`/api/reviews/${reviewId}`, { method: 'DELETE' });
+
+    //get current spot
+    const spotRes = await csrfFetch(`/api/spots/${spotId}`);
+    const currentSpot = await spotRes.json();
+
+    //update new avgRating
+    dispatch(getSpotByIdAction(currentSpot));
+
+    if (res.ok) {
+        dispatch(deleteReviewAction(reviewId))
+    }
+
+    const data = await res.json();
+    return data;
+}
+
 //reducer:
 const initialState = { spotReviews: {}, userReviews: {} }
+
 export default function reviewsReducer (state = initialState, action)  {
     switch (action.type) {
         case GET_REVIEWS_BY_SPOTID: {
@@ -33,6 +64,18 @@ export default function reviewsReducer (state = initialState, action)  {
             action.reviews.forEach(rev => newSpotReviews[rev.id] = rev);
             const newState = {...state, spotReviews: newSpotReviews}
             return newState;
+        }
+        case DELETE_REVIEW: {
+            //delete from spotReviews:
+            const newSpotReviews = {...state.spotReviews};
+            delete newSpotReviews[action.reviewId];
+
+            //delete from userReviews:
+            const newUserReviews = {...state.userReviews};
+            delete newUserReviews[action.reviewId];
+
+            return {...state, spotReviews: newSpotReviews, userReviews: newUserReviews}
+
         }
         default:
             return state;
