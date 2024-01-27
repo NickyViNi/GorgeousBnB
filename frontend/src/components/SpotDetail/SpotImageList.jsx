f399f076d8b1a2eb30afb32be6e67ff002620978
NickyViNi <happylemonlcm@gmail.com>
2024-01-26 18:09:42 -0800

    click image to expand

--- frontend/src/components/SpotDetail/SpotImageList.jsx
+++ frontend/src/components/SpotDetail/SpotImageList.jsx
@@ -1,25 +1,44 @@
+import { useState } from "react";
 
 export default function SpotImagesList ({spotImages}) {
     const mainImage = spotImages.find(img => img.preview);
     const subImages = spotImages.filter(img => !img.preview);
 
+    const [isFullScreen, setFullScreen] = useState(false);
+    const [fullScreenImage, setFullScreenImage] = useState(null);
+
+    const toggleFullScreen = (imgURL) => {
+        setFullScreen(!isFullScreen);
+        setFullScreenImage(imgURL);
+    }
+
+    const closeFullScreen = () => {
+        setFullScreen(false);
+    }
+
     return (
         <div className="spot-image-container">
-            <div id='main-image'>
-                <img src={mainImage?.url} />
+            <div id='main-image' onClick={() => toggleFullScreen(mainImage?.url)}>
+                <img src={mainImage?.url} alt="Spot Preview Image" />
             </div>
 
             <div id="sub-images-container">
                 {
                     subImages.map(img => {
                         return (
-                            <div key={img.id} className="sub-images">
-                                <img src={img?.url} />
+                            <div key={img.id} className="sub-images" onClick={() => toggleFullScreen(img?.url)}>
+                                <img src={img?.url} alt="Other Spot Image" />
                             </div>
                         )
                     })
                 }
             </div>
+            {isFullScreen && (
+                <div className="fullscreen-image-container" >
+                    <img src={fullScreenImage} alt="Spot Image" onClick={closeFullScreen} />
+                    <button onClick={closeFullScreen}>Close</button>
+                </div>
+            )}
         </div>
     )
 }
