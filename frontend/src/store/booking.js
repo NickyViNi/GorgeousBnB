402d780624bb1ab282f1bee92c174a28eb0371b0
NickyViNi <happylemonlcm@gmail.com>
2024-03-25 23:55:53 -0700

    bookings thunk

--- frontend/src/store/booking.js
+++ frontend/src/store/booking.js
@@ -2,12 +2,44 @@ import { csrfFetch } from "./csrf";
 
 //action types:
 const CREATE_BOOKING = "bookings/createBooking";
+const DELETE_BOOKING = "bookings/deleteBooking";
+const UPDATE_BOOKING = "bookings/updateBooking";
+const GET_BOOKINGS_BY_SPOTID = "bookings/getBookingBySpotId";
+const GET_CURRENT_USER_BOOKINGS = "bookings/getCurrentUserBookings";
 
 //action creator:
 export const createBookingAction = (booking) => {
     return {
         type: CREATE_BOOKING,
         booking
+    }
+}
+
+export const deleteBookingAction = (bookingId) => {
+    return {
+        type: DELETE_BOOKING,
+        bookingId
+    }
+}
+
+export const updateBookingAction = (booking) => {
+    return {
+        type: UPDATE_BOOKING,
+        booking
+    }
+}
+
+export const getBookingsBySpotIdAction = (bookings) => {
+    return {
+        type: UPDATE_BOOKING,
+        bookings
+    }
+}
+
+export const getCurrentUserBookingsAction = (bookings) => {
+    return {
+        type: UPDATE_BOOKING,
+        bookings
     }
 }
 
@@ -25,6 +57,72 @@ export const createBookingThunk = (booking, spotId) => async (dispatch) => {
     }
 
     return newBooking;
+}
+
+export const deleteBookingThunk = (bookingId) => async (dispatch) => {
+    //delete booking
+    const res = await csrfFetch(`/api/bookings/${bookingId}`, { method: 'DELETE' });
+
+    if (res.ok) {
+        dispatch(deleteBookingAction(bookingId))
+    }
+
+    const data = await res.json();
+    return data;
+}
+
+export const updateBookingThunk = (bookingId, updatedBooking, spotId) => async (dispatch) => {
+    // update booking:
+    const res = await csrfFetch(`/api/bookings/${bookingId}`, {
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(updatedBooking)
+    })
+    const booking = await res.json();
+
+    //get current spot bookings:
+    const resSpotBookings = await csrfFetch(`/api/spots/${spotId}/bookings`);
+    const boos = await resSpotBookings.json();
+    if(resSpotBookings.ok) {
+        const bookings = boos.bookings;
+        dispatch(getBookingsBySpotIdAction(bookings))
+    }
+
+    //get current user bookings:
+    const resBookings = await csrfFetch('/api/bookings/current');
+    const data = await resBookings.json();
+    if (resBookings.ok) {
+        const userBookings = data.bookings;
+        dispatch(getCurrentUserBookingsAction(userBookings));
+    }
+
+    if (res.ok) {
+        dispatch(updateBookingAction(booking));
+    }
+
+    return booking;
+}
+
+export const getBookingBySpotIdThunk = (bookingId) => async (dispatch) => {
+    const res = await csrfFetch(`/api/spots/${bookingId}/bookings`);
+
+    const data = await res.json();
+
+    if(res.ok) {
+        const bookings = data.bookings;
+        dispatch(getBookingsBySpotIdAction(bookings));
+    }
+    return data;
+}
+
+export const getCurrentUserBookingsThunk = (bookingId) => async (dispatch) => {
+    const res = await csrfFetch(`/api/spots/${bookingId}/bookings`);
+    const data = await res.json();
+    if (res.ok) {
+        const userBookings = data.bookings;
+        dispatch(getCurrentUserBookingsAction(userBookings));
+    }
+    return data;
 }
 
 //reducer:
