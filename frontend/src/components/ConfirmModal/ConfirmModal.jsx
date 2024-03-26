68aa03eb083f921fb257712342b937e67380c3cd
NickyViNi <happylemonlcm@gmail.com>
2024-03-26 14:47:38 -0700

    confirm modal

---
+++ frontend/src/components/ConfirmModal/ConfirmModal.jsx
@@ -0,0 +1,30 @@
+import "./ConfirmModal.css";
+
+function ConfirmFormModal({ header, text, deleteCb, cancelDeleteCb }) {
+  function Question() {
+    return <p>{text}</p>;
+  }
+
+  return (
+    <div id="confirm-delete">
+      <h2 className="subheading">{header}</h2>
+      <Question />
+      <div id="delete-btns">
+        <button
+          className="btn"
+          onClick={deleteCb}
+        >
+          <span>Yes</span>
+        </button>
+        <button
+          className="btn"
+          onClick={cancelDeleteCb}
+        >
+          <span>No</span>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ConfirmFormModal;
