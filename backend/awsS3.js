6b209b137f2af0b8ddc5b678732b0a1ab106bec6
NickyViNi <happylemonlcm@gmail.com>
2024-03-28 02:10:33 -0700

    only can upload first image

--- backend/awsS3.js
+++ backend/awsS3.js
@@ -2,7 +2,7 @@ const { aws } = require("./config");
 const AWS = require("aws-sdk");
 const multer = require("multer");
 const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
-const NAME_OF_BUCKET = aws;
+const NAME_OF_BUCKET = aws.NAME_OF_BUCKET;
 
 
 const singleFileUpload = async ({ file, public = false }) => {
